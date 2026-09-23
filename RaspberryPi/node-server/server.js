eb6e182eb7deb646d8141122c98a396f87d264ed
Bruno Pouliot <LeCanardNoir@users.noreply.github.com>
2026-09-23 15:50:39 -0400

    Add EndPoint START and STOP MILA

--- RaspberryPi/node-server/server.js
+++ RaspberryPi/node-server/server.js
@@ -1,7 +1,14 @@
 //server.js
-
 const express = require('express');
+const { util } = require('util');
+// Transforme exec en version basée sur les Promesses
+const exec = require('util').promisify(require('child_process').exec);
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+
+
 const app = express();
+const JETSON_IP_HOST = process.env.JETSON_IP_HOST;
 
 // handling CORS
 app.use((req, res, next) => {
@@ -15,6 +22,36 @@ app.use((req, res, next) => {
 // route for hello word
 app.get('/api/message', (req, res) => {
     res.send("HELLO WORLD !!!!");
+});
+
+app.get('/api/message', (req, res) => {
+    res.send("HELLO WORLD !!!!");
+});
+
+app.get("/mila/start", async (req, res) => {
+    try{
+        const { stdout, stderr } = await exec(`mosquitto_pub -h ${JETSON_IP_HOST} -t mila/control -m start_main_script`);
+
+        if (stderr) {
+            throw new Error(stderr);
+        }
+        res.status(200).json({ success: true, output: "Mila START !!!! Yeah!!!" });
+    }catch(error){
+        res.status(500).send("Un problème est survenu");
+    }
+});
+
+app.get("/mila/stop", async (req, res) => {
+    try{
+        const { stdout, stderr } = await exec(`mosquitto_pub -h ${JETSON_IP_HOST} -t mila/control -m stop_main_script`);
+
+        if (stderr) {
+            throw new Error(stderr);
+        }
+        res.status(200).json({ success: true, output: "Mila STOP!!! :(" });
+    }catch(error){
+        res.status(500).send("Un problème est survenu");
+    }
 });
 
 app.listen(3000, () => {
